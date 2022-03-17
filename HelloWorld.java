c1a144027473d78aa65aaaf1d102a822185a7696
madhu091995-1 <chmadhu091995@gmail.com>
2022-03-17 19:01:46 +0000

    added webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,19 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hai friend");
     System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
+    System.out.println("bye friend");
   }
 }

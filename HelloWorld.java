9de3fb4c85432feaa5625508206dae9575a0a62e
madhu091995-1 <chmadhu091995@gmail.com>
2022-03-17 18:11:41 +0000

    added something

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, baby --> good morning");
+    System.out.println("Hai friend");
+    System.out.println("bye friend");
   }
 }
